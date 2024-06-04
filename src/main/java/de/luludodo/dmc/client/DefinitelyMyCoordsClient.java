1d8458f4c439586eac254896f79f04489b2d7edf
agent LuluDodo <luludodo.gaming@gmail.com>
2024-06-04 14:16:42 +0200

    Fixed config

--- src/main/java/de/luludodo/dmc/client/DefinitelyMyCoordsClient.java
+++ src/main/java/de/luludodo/dmc/client/DefinitelyMyCoordsClient.java
@@ -1,8 +1,7 @@
 package de.luludodo.dmc.client;
 
 import de.luludodo.dmc.RelativeF3Coords;
-import de.luludodo.dmc.config.DMCConfig;
-import de.luludodo.dmc.config.JsonConfig;
+import de.luludodo.dmc.config.Config;
 import de.luludodo.dmc.keybinds.Keybindings;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
@@ -10,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class DefinitelyMyCoordsClient implements ClientModInitializer {
     public static final Logger log = LoggerFactory.getLogger("Definitely My Coords");
-    public static final JsonConfig config = new JsonConfig(new DMCConfig());
+    public static final Config config = new Config();
     @Override
     public void onInitializeClient() {
         Keybindings.register();
